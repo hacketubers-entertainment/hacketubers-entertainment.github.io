38eeb00f58dfecc59a77d6ba6bc5161e46d4fbdd
hacketubers-entertainment <129259430+hacketubers-entertainment@users.noreply.github.com>
2025-07-10 14:15:28 -0600

    fix: intento de arreglar estilos que no cargan

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://hacketubers-entertainment.github.io",
-  base: "hacketubers-entertainment.github.io",
+  base: "/",
 });
